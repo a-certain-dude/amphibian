b308df0231410199a052049226c85d0d128c3c8f
Jose <nkwantabisajoseph25@gmail.com>
2025-06-08 21:04:44 +0000

    create app container to hold dependencies

--- app/src/main/java/com/ahenkye/amphibians/data/AppContainer.kt
+++ app/src/main/java/com/ahenkye/amphibians/data/AppContainer.kt
@@ -1,6 +1,20 @@
 package com.ahenkye.amphibians.data
 
-import com.ahenkye.amphibians.network.Amphibians
+import com.ahenkye.amphibians.network.AmphibianApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/* Holds and provides dependencies */
 
 interface AppContainer {
+    val amphibiansRepository: AmphibiansRepository
+}
+
+class DefaultContainer : AppContainer {
+    private val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com"
+    private val retrofit =
+        Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(baseUrl)
+            .build()
+   private val retrofitService: AmphibianApiService by lazy { retrofit.create(AmphibianApiService::class.java) }
+    override val amphibiansRepository:AmphibiansRepository by lazy {NetworkAmphibianRepository(amphibianApiService = retrofitService)}
 }
